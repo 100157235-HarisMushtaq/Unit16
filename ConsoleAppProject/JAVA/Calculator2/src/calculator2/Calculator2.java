d2cfb55cd0ec825d486ac4c964b522e5419494b9
Haris Mushtaq <100157235@student.burnley.ac.uk>
2022-02-03 14:45:21 +0000

    start Calc 2

--- ConsoleAppProject/JAVA/Calculator2/src/calculator2/Calculator2.java
+++ ConsoleAppProject/JAVA/Calculator2/src/calculator2/Calculator2.java
@@ -9,12 +9,21 @@ package calculator2;
  * @author 100157235
  */
 public class Calculator2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    string name;
     
+    public int add(int x, int y){
+        return x + y;  
+    }
+    public int subtract(int x, int y){
+        return x - y;  
+    }
+    public int multiply(int x, int y){
+        return x * y;  
+    }
+    public int divide(int x, int y){
+        return x / y;  
+    }
+    public int modulus(int x, int y){
+        return x + y; 
+    }
 }
